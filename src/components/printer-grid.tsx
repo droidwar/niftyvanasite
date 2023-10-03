d92b94a16121702f1b9c4cb8aa59c45953e804ba
droidwar <76974855+droidwar@users.noreply.github.com>
2023-10-03 16:28:46 -0400

    fix another build issue

--- src/components/printer-grid.tsx
+++ src/components/printer-grid.tsx
@@ -8,7 +8,6 @@ export function PrinterGrid({printer}: { printer: Printer}){
             <h2 className="text-lg font-semibold mb-2">{printer.name}</h2>
             <p><strong>Price:</strong> ${printer.price}</p>
             <p><strong>Build Size:</strong> {printer.buildSize}mm</p>
-            <p><strong>Material:</strong> {printer.material}</p>
             <p><strong>Layer Resolution:</strong> {printer.layerResolution}mm</p>
             <p><strong>Connectivity:</strong> {printer.connectivity}</p>
             <p><strong>Heated Bed:</strong> {printer.heatedBed ? 'Yes' : 'No'}</p>
